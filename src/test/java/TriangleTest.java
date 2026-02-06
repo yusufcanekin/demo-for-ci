560a23cc530533a9139b95431a8e1806d7fe8019
amanda-zakir <amandazakir@gmail.com>
2026-02-06 14:02:20 +0100

    doc: triangle class and unit test documentation
    documentation added for triangle class and triangle unit test

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,7 +1,16 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * Unit test for Triangle class
+ * Checks if the area calculation is correct for a given base and height
+ */
 public class TriangleTest {
+    
+    /**
+     * Verifies that the calculateArea method returns the correct mathematical result.
+     * This test specifically checks a standard triangle with base 10 and height 5.
+     */
     @Test
     public void testCalculateArea() {
         Triangle triangle = new Triangle();
@@ -9,6 +18,10 @@ public class TriangleTest {
         double expected = 25.0;
         double actual = triangle.calculateArea(10.0, 5.0);
 
+        /**
+         * Asserts that the calculated value is within a 0.001 margin of error.
+         * This delta is necessary for floating-point comparisons in Java.
+         */
         assertEquals(expected, actual, 0.001);
     }
 }
